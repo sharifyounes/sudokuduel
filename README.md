6c3c94071ac4febb514b5ec5a5c3b740b794785d
Sharif Younes <sharif.m.younes@gmail.com>
2020-07-31 11:28:44 -0500

    scaffolding

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # sudokuduel
+
+To get started, make a virtualenv and activate it:
+
+```
+python3 -m venv .python3
+source .python/bin/activate
+```
+
+Add an alias
+
+```angular2
+alias sd="cd ~/Projects/sudokuduel; source .python3/bin/activate";
+```
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="sudokuduel",
+    version="1.0",
+    maintainer="Sharif Younes",
+    maintainer_email="sharif.m.younes@gmail.com",
+    packages=["app"],
+    install_requires=[
+        "Flask==1.1.2",
+    ],
+)
